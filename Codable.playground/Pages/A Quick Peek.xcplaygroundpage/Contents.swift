713cd6389ea57bf73a866a3093db47af42e99a15
Pasan Premaratne <pasan@premaratne.me>
2018-04-11 16:06:20 -0400

    S1V2

--- Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
@@ -12,6 +12,19 @@ struct Employee: Codable {
     let name: String
     let id: Int
     let role: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case id = "id"
+        case role = "role"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.role = try container.decode(String.self, forKey: .role)
+    }
 }
 
 let decoder = JSONDecoder()
