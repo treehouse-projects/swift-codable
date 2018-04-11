e735e2dcc8c334b037771fc3a192504010077f14
Pasan Premaratne <pasan@premaratne.me>
2018-04-11 16:33:09 -0400

    S1V5

--- Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/A Quick Peek.xcplaygroundpage/Contents.swift
@@ -4,45 +4,28 @@ let json = """
 {
     "name": "Pasan",
     "id": 1,
-    "role": "Teacher"
+    "role": "Teacher",
+    "start_date": "April 2012"
 }
 """.data(using: .utf8)!
 
-struct Employee: Decodable {
+struct Employee: Codable {
     let name: String
     let id: Int
     let role: String
-    
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case id = "id"
-        case role = "role"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.id = try container.decode(Int.self, forKey: .id)
-        self.role = try container.decode(String.self, forKey: .role)
-    }
+    let startDate: String
 }
 
 let decoder = JSONDecoder()
+decoder.keyDecodingStrategy = .convertFromSnakeCase
 
 let employee = try! decoder.decode(Employee.self, from: json)
 employee.name
 employee.id
 
-extension Employee: Encodable {
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-        try container.encode(id, forKey: .id)
-        try container.encode(role, forKey: .role)
-    }
-}
-
 let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
+
 let encodedEmployee = try encoder.encode(employee)
 
 print(encodedEmployee.stringDescription)
