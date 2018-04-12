e414ff127639f2cdc2ce3e6298a0a9cbcf5efa5c
Pasan Premaratne <pasan@premaratne.me>
2018-04-12 11:29:53 -0400

    S4V1

--- Codable.playground/Pages/Dynamic Keys.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/Dynamic Keys.xcplaygroundpage/Contents.swift
@@ -40,6 +40,48 @@ struct Language {
 
 struct Library {
     let languages: [Language]
+    
+    struct LibraryCodingKeys: CodingKey {
+        var stringValue: String
+        
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        
+        var intValue: Int? { return nil }
+        init?(intValue: Int) { return nil }
+        
+        static let languages = LibraryCodingKeys(stringValue: "languages")!
+    }
+    
+    enum LanguageCodingKeys: String, CodingKey {
+        case designer
+        case released
+    }
 }
 
 let decoder = JSONDecoder()
+
+extension Library: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: LibraryCodingKeys.self)
+        let languagesContainer = try container.nestedContainer(keyedBy: LibraryCodingKeys.self, forKey: .languages)
+        
+        self.languages = try languagesContainer.allKeys.map { key in
+            let languageContainer = try languagesContainer.nestedContainer(keyedBy: LanguageCodingKeys.self, forKey: key)
+            
+            let languageName = key.stringValue
+            let designer = try languageContainer.decode([String].self, forKey: .designer)
+            let releaseDate = try languageContainer.decode(String.self, forKey: .released)
+            
+            return Language(name: languageName, designer: designer, releaseDate: releaseDate)
+        }
+        
+    }
+}
+
+let library = try! decoder.decode(Library.self, from: json)
+
+for language in library.languages {
+    print(language.name)
+}
