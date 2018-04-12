e5295d31177e608635f103ed78ff11ab00f456d0
Pasan Premaratne <pasan@premaratne.me>
2018-04-12 10:52:36 -0400

    S3V1

--- Codable.playground/Pages/Wrapper Keys.xcplaygroundpage/Contents.swift
+++ Codable.playground/Pages/Wrapper Keys.xcplaygroundpage/Contents.swift
@@ -24,7 +24,49 @@ struct SearchResult {
     let booksCount: Int
     let ratingsCount: Int
     let textReviewsCount: Int
+    
+    enum OuterCodingKeys: String, CodingKey {
+        case work
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case booksCount
+        case ratingsCount
+        case textReviewsCount
+    }
+}
+
+extension SearchResult: Decodable {
+    init(from decoder: Decoder) throws {
+        let outerContainer = try decoder.container(keyedBy: OuterCodingKeys.self)
+        let innerContainer = try outerContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .work)
+        
+        self.id = try innerContainer.decode(Int.self, forKey: .id)
+        self.booksCount = try innerContainer.decode(Int.self, forKey: .booksCount)
+        self.ratingsCount = try innerContainer.decode(Int.self, forKey: .ratingsCount)
+        self.textReviewsCount = try innerContainer.decode(Int.self, forKey: .textReviewsCount)
+    }
 }
 
 let decoder = JSONDecoder()
 decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+let result = try! decoder.decode(SearchResult.self, from: json)
+
+extension SearchResult: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: OuterCodingKeys.self)
+        var innerContainer = container.nestedContainer(keyedBy: CodingKeys.self, forKey: .work)
+        
+        try innerContainer.encode(id, forKey: .id)
+        try innerContainer.encode(booksCount, forKey: .booksCount)
+        try innerContainer.encode(ratingsCount, forKey: .ratingsCount)
+        try innerContainer.encode(textReviewsCount, forKey: .textReviewsCount)
+    }
+}
+
+let encoder = JSONEncoder()
+encoder.keyEncodingStrategy = .convertToSnakeCase
+
+print(try! encoder.encode(result).stringDescription)
